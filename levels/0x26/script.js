5e9c933bd24fb95ef58901018dcdb3066959df21
Solana <solana.raxhon.an123@gmail.com>
2020-03-20 16:20:28 +0100

    0xDB-0xF1-0xF5-0xB2-0x7E-0x6A0x56-0x4A-0x26-0x0D
    Niveaux 0xDB-0xF1-0xF5-0xB2-0x7E-0x6A0x56-0x4A-0x26-0x0D

---
+++ levels/0x26/script.js
@@ -0,0 +1,21 @@
+/*----------------------------------------*\
+  256^3 - script.js
+  @author Evrard Vincent (vincent@ogre.be)
+  @Date:   2020-02-06 23:31:08
+  @Last Modified time: 2020-02-06 23:40:40
+\*----------------------------------------*/
+
+AppManager.ready(function(){
+	InteractionHelper.onLongTap("#monImage1", eventHandler);
+	});
+
+function eventHandler(event){
+	console.log(event);
+	document.querySelector("#monImage1").classList.add("hide");
+	document.querySelector("#monImage2").classList.remove("hide");
+	InteractionHelper.onMouseLeave("#monImage2", eventHandler2);
+	AppManager.levelComplete();
+}
+function eventHandler2(event){
+	document.querySelector("#monImage2").classList.add("hide");
+	}
