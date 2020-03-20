7c379293b76f291f2e3c36721c50892ac3476197
Akif Türkel <akif.turkel.an123@gmail.com>
2020-03-20 15:17:08 +0100

    0xAA - 0xD0 - 0xE4 - 0xDA - 0x2D - 0xC8 - 0xDE - 0xED - 0xD9 - 0x48
    0xAA - 0xD0 - 0xE4 - 0xDA - 0x2D - 0xC8 - 0xDE - 0xED - 0xD9 - 0x48

---
+++ levels/0x48/script.js
@@ -0,0 +1,28 @@
+/*----------------------------------------*\
+  256^3 - script.js
+  @author Evrard Vincent (vincent@ogre.be)
+  @Date:   2020-02-06 23:31:08
+  @Last Modified time: 2020-02-06 23:43:17
+\*----------------------------------------*/
+
+AppManager.ready(function(){
+	InteractionHelper.onDropFile("#Image1", dfile);
+});
+
+function dfile(event){
+	console.log(event);
+  
+    document.querySelector("#Image1").classList.add ("hide");
+    document.querySelector("#Image2").classList.remove("hide");
+
+InteractionHelper.onKeyDown(keypressed);
+	
+}
+
+
+function keypressed(event){
+	console.log(event);
+    if(event.key == "Delete")
+	AppManager.levelComplete();
+
+}
---
+++ levels/0xD0/script.js
@@ -0,0 +1,29 @@
+/*----------------------------------------*\
+  256^3 - script.js
+  @author Evrard Vincent (vincent@ogre.be)
+  @Date:   2020-02-06 23:31:08
+  @Last Modified time: 2020-02-06 23:43:02
+\*----------------------------------------*/
+
+AppManager.ready(function(){
+	InteractionHelper.onScroll("#Image1", scrolled);
+});
+
+function scrolled(event){
+	console.log(event);
+     
+    document.querySelector("#Image1").classList.add ("hide");
+    document.querySelector("#Image2").classList.remove("hide");
+
+
+
+InteractionHelper.onTap("#Image2", tap);
+	
+}
+
+
+function tap(event){
+	console.log(event);
+	AppManager.levelComplete();
+
+}
