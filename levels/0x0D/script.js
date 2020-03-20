8d5a6981c7ca363bc94f2a21774c14f91468c2b4
Fabian <56698917+Soya1999@users.noreply.github.com>
2020-03-20 19:12:38 +0100

    Update script.js

--- levels/0x0D/script.js
+++ levels/0x0D/script.js
@@ -20,7 +20,7 @@ function eventHandlerAscenseur(event){
 
 function eventHandlerAlien(event){
     console.log(document.querySelector("#Jean_Michel").scrollTop);
-    if(document.querySelector("#Jean_Michel").scrollTop > 470) {
+    if(document.querySelector("#Jean_Michel").scrollTop > 160) {
         AppManager.levelComplete();
         
     }
