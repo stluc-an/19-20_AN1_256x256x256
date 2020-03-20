4d097df8855c44176346b0ad7f0416b86d6eec39
Fabian <56698917+Soya1999@users.noreply.github.com>
2020-03-20 18:55:32 +0100

    Update script.js

--- levels/0xFE/script.js
+++ levels/0xFE/script.js
@@ -13,6 +13,14 @@ function eventHandler(event){
     console.log(event);
 	if(event.intensity>20){
         document.querySelector("#Sleep2").classList.remove("hide");
+        
+    InteractionHelper.onTimeout("#Sleep2", wait);
+        
     }
-	AppManager.levelComplete();
+}
+
+function wait(eventHandler){
+    console.log(eventHandler);
+    setTimeout(AppManager.levelComplete(), 2000);
+    
 }
