06233be61318437a473a14f02414783cc92b6d92
Fabian <56698917+Soya1999@users.noreply.github.com>
2020-03-21 15:09:05 +0100

    t

--- levels/0x0D/script.js
+++ levels/0x0D/script.js
@@ -15,7 +15,7 @@ function eventHandlerAscenseur(event){
     document.querySelector("#ascenseur2").classList.remove("hide");
     document.querySelector("#Jean_Michel").classList.remove("hide");
     
-    setTimeout (pause, 6000);
+    setTimeout (pause, 8000);
      
 }
 
