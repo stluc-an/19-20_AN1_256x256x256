cb690bcb8b8b611fbe17edb50073d856017abe46
oogre <vincent@ogre.be>
2020-03-21 04:17:43 +0100

    debug

--- Recherches/cov.brick/script.js
+++ Recherches/cov.brick/script.js
@@ -2,7 +2,7 @@
   256^3 - script.js
   @author Evrard Vincent (vincent@ogre.be)
   @Date:   2020-02-06 23:31:08
-  @Last Modified time: 2020-03-21 04:07:22
+  @Last Modified time: 2020-03-21 04:17:24
 \*----------------------------------------*/
 const __CHAR_PER_LINE__ = 16;
 let player;
@@ -62,8 +62,7 @@ function update(time){
 		ball.start();
 		ball.isRunning = true;
 	}
-		
-	//console.log();
-
+	
+	console.log("TEST")
 	requestAnimationFrame(update);
 }
