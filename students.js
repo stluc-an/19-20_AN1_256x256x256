92c8f585f6df41da24303fb136ab1da49545d4aa
oogre <vincent@ogre.be>
2020-03-29 04:19:53 +0200

    debug css + test student

--- students.js
+++ students.js
@@ -4,195 +4,223 @@ const students = [
 		name : "Gaëtan Angheben",
 		levels : [
 			"0xFB", "0xD1", "0x5B", "0xFC", "0x5C", "0x38", "0x86", "0x0F", "0xE3", "0x00"
-		]
+		],
+		pullRequest : [ 3, 8, 39 ]
 	}, 
 	{
 		acronym : "EBE", 
 		name : "Elisa Bertrand",
 		levels : [
 			"0xD4", "0x0A_bis", "0x52", "0x90", "0x1E", "0xDC", "0x16", "0x27", "0x0E_bis", "0xD6"
-		]
+		],
+		pullRequest : [ ]
 	}, 
 	{
 		acronym : "MCO", 
 		name : "Martin Coets",
 		levels : [
 			"0x03_bis", "0x1F", "0x94", "0x19", "0x47", "0x08_bis", "0xE1", "0xF7", "0xD7", "0x3F"
-		]
+		],
+		pullRequest : [ ]
 	}, 
 	{
 		acronym : "TCO", 
 		name : "Tamara Colin",
 		levels : [
 			"0x78", "0x32", "0x3E", "0x14_bis", "0x3D", "0x29", "0x2F", "0xFF", "0x3C", "0x9C"
-		]
+		],
+		pullRequest : [ 25, 48, 51 ]
 	}, 
 	{
 		acronym : "FHC", 
 		name : "Fabian D'Hondt",
 		levels : [
 			"0xE5", "0x39", "0x7A", "0xFA", "0x0D", "0xFE", "0x3B", "0xF3", "0x79", "0x8E"
-		]
+		],
+		pullRequest : [ 6, 11, 21 ]
 	}, 
 	{
 		acronym : "ADA", 
 		name : "Anahy Dainville",
 		levels : [
 			"0x36", "0x06_bis", "0x8D", "0x15_bis", "0x80", "0x01_bis", "0xE2", "0xE6", "0xEE", "0x4C"
-		]
+		],
+		pullRequest : [ 29, 49, 52, 55 ]
 	}, 
 	{
 		acronym : "LDE", 
 		name : "Luanna Deltenre",
 		levels : [
 			"0x54", "0xAD", "0xF9", "0x11", "0x63", "0x13", "0xFD", "0x88", "0x00_bis", "0x12"
-		]
+		],
+		pullRequest : [ 16, 17, 24, 34 ]
 	}, 
 	{
 		acronym : "JDE", 
 		name : "Jolan Dessoy",
 		levels : [
 			"0x62", "0x35", "0x0F_bis", "0xA1", "0x95", "0x9B", "0x33", "0x66", "0x28", "0x20"
-		]
+		],
+		pullRequest : [ 19, 22 ]
 	}, 
 	{
 		acronym : "TDR", 
 		name : "Titouan Dresse",
 		levels : [
 			"0x02", "0x51", "0x8F", "0xA5", "0x09_bis", "0x6B", "0x85", "0x9F", "0x68", "0x05"
-		]
+		],
+		pullRequest : [ 35, 38 ]
 	}, 
 	{
 		acronym : "AEH", 
 		name : "Adil El Hassouni",
 		levels : [
 			"0x0B_bis", "0x53", "0xA0", "0xAC", "0x12_bis", "0x84", "0xAE", "0x69", "0x97", "0x99"
-		]
+		],
+		pullRequest : [ 45, 58, 61, 62 ]
 	}, 
 	{
 		acronym : "AFR", 
 		name : "Augustin Fraioli",
 		levels : [
 			"0x83", "0x4E", "0x50", "0x55", "0xBD", "0x10", "0x98", "0x10_bis", "0x9E", "0x9D"
-		]
+		],
+		pullRequest : [ 40, 60 ]
 	}, 
 	{
 		acronym : "HHA", 
 		name : "Hiba Hajji",
 		levels : [
 			"0x11_bis", "0x60", "0xB6", "0x92", "0xA2", "0x42", "0x07_bis", "0x61", "0x8A", "0x08"
-		]
+		],
+		pullRequest : [ ]
 	}, 
 	{
 		acronym : "THE", 
 		name : "Thomas Heyvaert",
 		levels : [
 			"0x04_bis", "0x0B", "0x8B", "0x87", "0x9A", "0x41", "0x25", "0x64", "0x82", "0x91"
-		]
+		],
+		pullRequest : [ ]
 	}, 
 	{
 		acronym : "LKN", 
 		name : "Lina Kizodisa Nkoko",
 		levels : [
 			"0xAF", "0x17", "0x65", "0x22", "0x89", "0x81", "0x93", "0x4F", "0x49", "0x23"
-		]
+		],
+		pullRequest : [ 2, 7, 33, 36, 46 ]
 	}, 
 	{
 		acronym : "ELY", 
 		name : "Eleni Lykou",
 		levels : [
 			"0x0C_bis", "0x02_bis", "0x70", "0xB8", "0x2A", "0x46", "0x5D", "0x18", "0xEB", "0xF0"
-		]
+		],
+		pullRequest : [ ]
 	}, 
 	{
 		acronym : "ANI", 
 		name : "Arthur Nicaise",
 		levels : [
 			"0x4D", "0x58", "0x31", "0xB4", "0x67", "0x45", "0x0A", "0xB0", "0x1B", "0xD8"
-		]
+		],
+		pullRequest : [ 13 ]
 	}, 
 	{
 		acronym : "ANN", 
 		name : "Achille Njomeleu Ngassa",
 		levels : [
 			"0xCA", "0x57", "0x13_bis", "0xBC", "0x4B", "0xE0", "0x72", "0xCC", "0x43", "0x07"
-		]
+		],
+		pullRequest : [ ]
 	}, 
 	{
 		acronym : "QPO", 
 		name : "Quentin Poujois",
 		levels : [
 			"0xA7", "0x09", "0xE8", "0x5A", "0x17_bis", "0x15", "0xCB", "0x76", "0x8C", "0x6E"
-		]
+		],
+		pullRequest : [ 12, 32 ]
 	}, 
 	{
 		acronym : "SRA", 
 		name : "Solana Raxhon",
 		levels : [
 			"0x7E", "0x26", "0xF1", "0xDB", "0xF5", "0x6A", "0x4A", "0x56", "0xB2", "0x0D_bis"
-		]
+		],
+		pullRequest : [ 15, 18, 23 ]
 	}, 
 	{
 		acronym : "MSA", 
 		name : "Milo Salza",
 		levels : [
 			"0xAB", "0xD3", "0xCD", "0x3A", "0x6D", "0x40", "0x7B", "0x2C", "0xBB", "0xE7"
-		]
+		],
+		pullRequest : [ 27, 53, 54, 56, 57 ]
 	}, 
 	{
 		acronym : "CMG", 
 		name : "Carol San Martin y Gomez",
 		levels : [
 			"0xE9", "0x7C", "0xC4", "0x7D", "0x5E", "0x6C", "0x04", "0xC6", "0x7F", "0xC1"
-		]
+		],
+		pullRequest : [ 5, 9, 20 ]
 	}, 
 	{
 		acronym : "JSE", 
 		name : "Julie Servais",
 		levels : [
 			"0x16_bis", "0x2B", "0xB3", "0x77", "0xA6", "0x0E", "0xEC", "0xCE", "0xEA", "0xB1"
-		]
+		],
+		pullRequest : [ ]
 	}, 
 	{
 		acronym : "TSI", 
 		name : "Tommaso Simonini",
 		levels : [
 			"0xEF", "0xF8", "0xB9", "0x6F", "0xC5", "0x06", "0xC9", "0xC2", "0xA4", "0x73"
-		]
+		],
+		pullRequest : [ 4, 10, 28, 31, 41, 59 ]
 	}, 
 	{
 		acronym : "AST", 
 		name : "Arthur Steenhoudt",
 		levels : [
 			"0x14", "0x71", "0x2E", "0xB5", "0xA8", "0x44", "0xF6", "0x59", "0x5F", "0x05_bis"
-		]
+		],
+		pullRequest : [ 30, 43, 44, 47 ]
 	}, 
 	{
 		acronym : "STO", 
 		name : "Serena Toseda",
 		levels : [
 			"0xA3", "0xC3", "0xDF", "0x0C", "0x74", "0xBE", "0x75", "0xBA", "0xC0", "0xF2"
-		]
+		],
+		pullRequest : [ ]
 	}, 
 	{
 		acronym : "STR", 
 		name : "Steven Trinidad",
 		levels : [
 			"0xA9", "0xF4", "0x24", "0xBF", "0xD2", "0xDD", "0x1A", "0xC7", "0xCF", "0xB7"
-		]
+		],
+		pullRequest : [ ]
 	}, 
 	{
 		acronym : "ATÜ", 
 		name : "Akif Türkel",
 		levels : [
 			"0xAA", "0xD0", "0xE4", "0xDA", "0x2D", "0xC8", "0xDE", "0xED", "0xD9", "0x48"
-		]
+		],
+		pullRequest : [ 26, 50 ]
 	},
 	{
 		acronym : "MVA", 
 		name : "Matthieu Vanhecke",
 		levels : [
 			"0xD5", "0x1D", "0x03", "0x21", "0x1C", "0x96", "0x30", "0x37", "0x34", "0x01"
-		]
+		],
+		pullRequest : [ 37, 42 ]
 	}
 ]
